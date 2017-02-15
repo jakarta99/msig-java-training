70f01f0b176a7fb852e98b6f315ba31b972a6187
Gary Lee <jakarta99@gmail.com>
2017-02-15 11:08:18 +0800

    Hello Java Application

---
+++ lesson1/src/tw/com/msig/app/HelloApp.java
@@ -0,0 +1,14 @@
+package tw.com.msig.app;
+
+public class HelloApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello "+" MSIG !");
+
+		System.out.println(1+1);
+		
+		System.out.println("1+1="+1+1);
+		System.out.println("1+1="+(1+1));
+	}
+
+}
